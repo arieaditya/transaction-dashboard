089fcc2221388bfaa11f8495565e256e3d093659
arieadityan <arie@hypefast.id>
2026-05-05 08:29:20 +0700

    chore: scaffold nuxt app and starter files

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="py-8">
+        <h1>Transaction Dashboard</h1>
+        <v-btn color="primary" class="mt-4">
+          Vuetify is working
+        </v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
